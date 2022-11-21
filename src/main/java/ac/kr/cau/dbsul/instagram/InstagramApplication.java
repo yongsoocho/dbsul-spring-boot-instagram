2e4457dc158f204acafee7a7cf7db442ad76273d
yongsoocho <yongsoocho578@gmail.com>
2022-11-21 16:22:49 +0900

    #1 snippets

---
+++ src/main/java/ac/kr/cau/dbsul/instagram/InstagramApplication.java
@@ -0,0 +1,13 @@
+package ac.kr.cau.dbsul.instagram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstagramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstagramApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ac/kr/cau/dbsul/instagram/InstagramApplicationTests.java
@@ -0,0 +1,13 @@
+package ac.kr.cau.dbsul.instagram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstagramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
